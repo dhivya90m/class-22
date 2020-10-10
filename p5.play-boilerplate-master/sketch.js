a1c5db2ee850eddb3a553fd82d97976fda7deb99
dhivya90m <69674869+dhivya90m@users.noreply.github.com>
2020-10-10 08:18:03 +0530

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -2,30 +2,45 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 
-
 var engine, world;
-var object;
-
+var ground,ball;
 function setup() {
   createCanvas(400,400);
-  
   engine = Engine.create();
   world = engine.world;
+  
+  var ground_options={
+   isStatic: true
 
-  object = Bodies.rectangle(200,100,50,50);
-  World.add(world,object);
+  }
 
-  console.log(object);
-  console.log(object.position.x);
-  console.log(object.position.y);
+  ground = Bodies.rectangle(200,390,200,20,ground_options);
+  World.add(world,ground);
+
+  console.log(ground);
+
+
+  ball_options={
+    restitution: 1.0
+
+  }
+  ball = Bodies.circle(200,100,20,ball_options);
+  World.add(world,ball);
+
+
+
 
 }
 
 function draw() {
-  background(0,0,0); 
-  //rectMode(CENTER);
-  rect(200,200,50,50);
-  rect(object.position.x,object.position.y,50,50);
+  background(0,0,0);  
+  Engine.update(engine);
+  fill("brown");
+rectMode(CENTER);
+rect(ground.position.x,ground.position.y,400,20);
 
+fill("blue");
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,20,20);
   drawSprites();
 }
